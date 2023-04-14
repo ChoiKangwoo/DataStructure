89b26630c9055f7af6c1bb5d0a3e0ccea26559ba
Kang Woo Choi <rkddn6464@gmail.com>
2023-04-14 21:49:17 +0900

    다항식의 표현방법(모든계수저장)

---
+++ 과제/다항식의 표현방법1.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(a,b) (((a)<(b)?(a):(b)))
+#define MAX_DEGREE 101
+
+typedef struct {
+	int degree;
+	float coef[MAX_DEGREE];
+}polynomial;
+
+polynomial poly_add1(polynomial A, polynomial B)
+{
+	polynomial C;
+	int Apos = 0, Bpos = 0, Cpos = 0;
+	int degree_a = A.degree;
+	int degree_b = B.degree;
+	C.degree = MAX(A.degree, B.degree);
+	while (Apos <= A.degree&&Bpos <= B.degree) {
+		if (degree_a > degree_b) {
+			C.coef[Cpos++] = A.coef[Apos++];
+			degree_a--;
+		}
+		else if (degree_a == degree_b) {
+			C.coef[Cpos++] = A.coef[Apos++] + B.coef[Bpos++];
+			degree_a--;
+			degree_b--;
+		}
+		else {
+			C.coef[Cpos++] = B.coef[Bpos++];
+			degree_b--;
+		}
+	}
+	return C;
+}
+
+int main()
+{
+	polynomial a = { 5,{3,6,0,0,0,10} };
+	polynomial b = { 4,{7,0,5,0,1} };
+	polynomial c;
+	c = poly_add1(a, b);
+	for (int i = 0; i <= MAX(a.degree, b.degree)+1; i++)
+	{
+		printf("%f  ", c.coef[i]);
+	}
+}
